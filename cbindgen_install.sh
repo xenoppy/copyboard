741b1ab5042f9ea2301f1202db0f0e8daa2a28f4
xenoppy <1340319151@qq.com>
2023-07-13 23:30:28 +0800

    update

--- cbindgen_install.sh
+++ cbindgen_install.sh
@@ -2,7 +2,7 @@
  # @Author: xenoppy
  # @Date: 2023-07-13 22:39:10
  # @LastEditors: xenoppy 1340319151@qq.com
- # @LastEditTime: 2023-07-13 22:42:01
+ # @LastEditTime: 2023-07-13 23:29:54
  # @FilePath: /copyboard/cbindgen_install.sh
  # @Description: 有任何问题请与我联系
 ### 
@@ -10,6 +10,7 @@
 wget https://github.com/mozilla/cbindgen/archive/v0.24.3/cbindgen-0.24.3.tar.gz
 tar xvf cbindgen-0.24.3.tar.gz
 echo "entering cbindgen-0.24"
+cp cargo_config /root/.cargo/config
 cd cbindgen-0.24.3
 vim Cargo.lock
 cargo build --release
